beb1682061eb08424b7d15c620915687052d9ff8
Angelo Postiglione <anpostiglione@gmail.com>
2013-12-22 00:51:32 +0100

    update

--- Codility/src/org/apostigl/codility/ex1/Ex1_SmallFrog.java
+++ Codility/src/org/apostigl/codility/ex1/Ex1_SmallFrog.java
@@ -7,15 +7,15 @@ package org.apostigl.codility.ex1;
  */
 
 public class Ex1_SmallFrog {
-	
+
 	public int solution(int X, int Y, int D) {
-	       
-    int num = Y-X;
 
-	int x = num % D;
-	int r = num / D;
+		int num = Y - X;
 
-	return x == 0 ? r : r+1;
-    }
+		int x = num % D;
+		int r = num / D;
+
+		return x == 0 ? r : r + 1;
+	}
 }
 
